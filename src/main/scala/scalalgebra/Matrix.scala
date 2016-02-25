e602d8e3af2ce839f692d419742a9cf8f007a437
Aleksey Dolgii <dolgii@devexperts.com>
2016-02-25 10:49:29 +0300

    equals

--- src/main/scala/scalalgebra/Matrix.scala
+++ src/main/scala/scalalgebra/Matrix.scala
@@ -2,19 +2,18 @@ package scalalgebra
 
 class Matrix(elems: Vector[Vector[Double]]) {
   val rows = elems.length
-
   require(rows > 0)
   require(elems forall (r => r.length == elems.head.length))
-
   val cols = elems.head.length
-
   require(cols > 0)
-
   private val data: Vector[Double] = elems.flatten
-
   val size = data.length
 
-  override def equals(obj: scala.Any): Boolean = true
+  override def equals(that: scala.Any): Boolean = that match {
+    case m: Matrix =>
+      m.cols == cols && m.data == data
+    case _ => false
+  }
 }
 
 object Matrix {
