c919c15f94c24a9ddcabc0e4977cf7053d17c985
Aleksey Dolgii <dolgii@devexperts.com>
2016-03-01 12:34:27 +0300

    store all in 1 dimensional vector

--- src/main/scala/scalalgebra/Matrix.scala
+++ src/main/scala/scalalgebra/Matrix.scala
@@ -86,8 +86,7 @@ class Matrix(val elements: Vector[Double], val rows: Int, val cols: Int, val pre
 
   override def equals(that: scala.Any): Boolean = that match {
     case m: Matrix => compareSize(m) && {
-      //todo refactor?
-      m.data.flatten zip data.flatten forall (p => (p._1 - p._2).abs <= precision)
+      m.elements zip elements forall (p => (p._1 - p._2).abs <= precision)
     }
     case _ => false
   }
